cdc831b97124f22d92b44012331faa8ffc60d29e
Fachri Hawari <fachri.hawari@gmail.com>
2025-03-04 13:44:53 +0700

    feat: Login Done

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,9 @@ class AuthController {
   static async register(req, res) {
     res.send('Register');
   }
+  static login(req, res) {
+    res.send('Login')
+  }
 }
 
 module.exports = AuthController
--- router/auth.js
+++ router/auth.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/auth');
 const authRouter = express.Router();
 
 authRouter.post('/register', AuthController.register);
+authRouter.post('/login', AuthController.login)
 
 module.exports = authRouter
