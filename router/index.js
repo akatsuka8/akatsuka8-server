ad18a6a1dbe067e7cbacb7012321dd6b136601ec
Fachri Hawari <fachri.hawari@gmail.com>
2025-03-04 13:27:04 +0700

    feat: setup express done

---
+++ router/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+
+module.exports = indexRouter
